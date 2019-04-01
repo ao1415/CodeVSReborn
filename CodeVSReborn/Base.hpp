c26daebc20a7e880c5eaeb6624391eb98f79a551
ao1415 <ao14159265@gmail.com>
2019-04-02 00:28:05 +0900

    シミュレータテスト前

--- CodeVSReborn/Base.hpp
+++ CodeVSReborn/Base.hpp
@@ -16,25 +16,32 @@
 #include <chrono>
 #include <memory>
 
+#include <cmath>
+
 #include "FixedGrid.hpp"
 #include "Clock.hpp"
+
+using Num = int;
 
 constexpr int DangerLine = 2;;
 
 constexpr int Witdh = 10;
-constexpr int Height = 16 + 2;
+constexpr int Height = 16 + DangerLine;
 
 constexpr int MaxTurn = 500;
 constexpr int PackSize = 2;
 
 constexpr int PackDropRange = 8;
+constexpr Num Empty = 0;
+constexpr int Inf = 99;
+
+constexpr Num Elimination = 10;
 
 const std::string Skill = "S";
 
-using Num = int;
-
 using PackArray = FixedGrid<Num, PackSize, PackSize>;
 using FieldArray = FixedGrid<Num, Witdh, Height>;
+using BitFieldArray = FixedGrid<bool, Witdh, Height>;
 
 inline const Num readNum() {
 	int v;
@@ -56,3 +63,14 @@ struct Command {
 	}
 
 };
+
+struct Chain {
+
+	Chain(int c, int s) : chain(c), score(s) {}
+
+	int chain = 0;
+	int score = 0;
+};
+
+inline bool inside(const int& v, const int& max) { return (0 <= v && v < max); }
+inline bool inside(const std::pair<int, int>& pos) { return (inside(pos.first, Witdh) && inside(pos.second, Height)); }
--- CodeVSReborn/Field.hpp
+++ CodeVSReborn/Field.hpp
@@ -7,22 +7,356 @@ class Field {
 private:
 
 	FieldArray table;
+	//横・右下・下・左下
+	std::array<FieldArray, 4> cumulative;
+	std::array<Num, Witdh> elevation;
+
+	void setElevation() {
+
+		for (int x = 0; x < Witdh; x++)
+		{
+			for (int y = DangerLine; y < Height; y++)
+			{
+				if (table[y][x] != Empty)
+				{
+					elevation[x] = y - 1;
+					break;
+				}
+			}
+		}
+
+	}
+
+	void setCumulative() {
+
+		int dx[] = { 1,1,0,-1 };
+		int dy[] = { 0,1,1,1 };
+
+		for (int y = 0; y < Height; y++)
+		{
+			for (int d = 0; d < 4; d++)
+				cumulative[d][y][0] = table[y][0];
+		}
+		for (int x = 1; x < Witdh; x++)
+		{
+			for (int d = 0; d < 4; d++)
+				cumulative[d][0][x] = table[0][x];
+		}
+
+		for (int y = 1; y < Height; y++)
+		{
+			for (int x = 1; x < Witdh; x++)
+			{
+				for (int d = 0; d < 4; d++)
+				{
+					int px = x - dx[d];
+					int py = y - dy[d];
+
+					if (table[y][x] != Empty)
+					{
+						cumulative[d][y][x] = cumulative[d][py][px] + table[y][x];
+					}
+					else
+					{
+						cumulative[d][y][x] = cumulative[d][py][px] + Inf;
+					}
+				}
+			}
+		}
+	}
+
+	bool eraseBlock() {
+
+		int count = 0;
+
+		//横・右下・下・左下
+		int dx[] = { 1,1,0,-1 };
+		int dy[] = { 0,1,1,1 };
+
+		BitFieldArray bitField(false);
+
+		//横
+		for (int y = 0; y < Height; y++)
+		{
+			int w = 1;
+			for (int x = 0; x < Witdh; x++)
+			{
+				while (x + w < Witdh)
+				{
+					const auto sub = cumulative[0][y][x + w] - cumulative[0][y][x] + table[y][x];
+
+					if (sub < Elimination)
+					{
+						w++;
+					}
+					if (sub > Elimination)
+					{
+						w = std::max(1, w - 1);
+					}
+					else
+					{
+						for (int p = x; p <= x + w; p++)
+						{
+							bitField[y][p] = true;
+						}
+						break;
+					}
+				}
+			}
+		}
+
+		//横
+		for (int x = 0; x < Witdh; x++)
+		{
+			int h = 1;
+			for (int y = elevation[x]; y < Height; y++)
+			{
+				while (y + h < Height)
+				{
+					const auto sub = cumulative[2][y + h][x] - cumulative[2][y][x] + table[y][x];
+
+					if (sub < Elimination)
+					{
+						h++;
+					}
+					if (sub > Elimination)
+					{
+						h = std::max(1, h - 1);
+					}
+					else
+					{
+						for (int p = y; p <= y + h; p++)
+						{
+							bitField[p][x] = true;
+						}
+						break;
+					}
+				}
+			}
+		}
+
+		//右下
+		for (int s = 0; s < Witdh; s++)
+		{
+			int x = s;
+			int y = 0;
+
+			int w = 1;
+			for (int i; i < Height; i++)
+			{
+				x++;
+				y++;
+				while (inside(x + w, y + w))
+				{
+					const auto sub = cumulative[1][y + w][x + w] - cumulative[1][y][x] + table[y][x];
+
+					if (sub < Elimination)
+					{
+						w++;
+					}
+					if (sub > Elimination)
+					{
+						w = std::max(1, w - 1);
+					}
+					else
+					{
+						for (int p = 0; p <= w; p++)
+						{
+							bitField[y + p][x + p] = true;
+						}
+						break;
+					}
+				}
+			}
+		}
+		for (int s = 1; s < Height; s++)
+		{
+			int x = 0;
+			int y = s;
+
+			int w = 1;
+			for (int i; i < Height; i++)
+			{
+				x++;
+				y++;
+				while (inside(x + w, y + w))
+				{
+					const auto sub = cumulative[1][y + w][x + w] - cumulative[1][y][x] + table[y][x];
+
+					if (sub < Elimination)
+					{
+						w++;
+					}
+					if (sub > Elimination)
+					{
+						w = std::max(1, w - 1);
+					}
+					else
+					{
+						for (int p = 0; p <= w; p++)
+						{
+							bitField[y + p][x + p] = true;
+						}
+						break;
+					}
+				}
+			}
+		}
+
+		//左下
+		for (int s = 1; s < Witdh; s++)
+		{
+			int x = s;
+			int y = 0;
+
+			int w = 1;
+			for (int i; i < Height; i++)
+			{
+				x--;
+				y++;
+				while (inside(x + w, y + w))
+				{
+					const auto sub = cumulative[1][y + w][x + w] - cumulative[1][y][x] + table[y][x];
+
+					if (sub < Elimination)
+					{
+						w++;
+					}
+					if (sub > Elimination)
+					{
+						w = std::max(1, w - 1);
+					}
+					else
+					{
+						for (int p = 0; p <= w; p++)
+						{
+							bitField[y + p][x + p] = true;
+						}
+						break;
+					}
+				}
+			}
+		}
+		for (int s = 1; s < Height; s++)
+		{
+			int x = Witdh - 1;
+			int y = s;
+
+			int w = 1;
+			for (int i; i < Height; i++)
+			{
+				x++;
+				y++;
+				while (inside(x + w, y + w))
+				{
+					const auto sub = cumulative[1][y + w][x + w] - cumulative[1][y][x] + table[y][x];
+
+					if (sub < Elimination)
+					{
+						w++;
+					}
+					if (sub > Elimination)
+					{
+						w = std::max(1, w - 1);
+					}
+					else
+					{
+						for (int p = 0; p <= w; p++)
+						{
+							bitField[y + p][x + p] = true;
+						}
+						break;
+					}
+				}
+			}
+		}
+
+		bool isErase = false;
+		for (int y = 0; y < Height; y++)
+		{
+			for (int x = 0; x < Witdh; x++)
+			{
+				if (bitField[y][x])
+				{
+					table[y][x] = Empty;
+					isErase = true;
+				}
+			}
+		}
+
+		return isErase;
+	}
+
+	void dropBlock() {
+
+		for (int x = 0; x < Witdh; x++)
+		{
+			const int h = elevation[x];
+			int index = Height - 1;
+
+			for (int y = Height - 1; y >= h; y--)
+			{
+				if (table[y][x] != Empty)
+				{
+					if (index != y)
+					{
+						table[index][x] = table[y][x];
+						table[y][x] = Empty;
+					}
+					index--;
+				}
+			}
+		}
+
+	}
 
 public:
 
 	Field() {
+
 		table.fill(0);
+
 	}
 
 	void setNextField(const Pack& pack, const Command& command) {
 
+		const auto& pos = command.pos;
+		const auto& r = command.rotate;
+
+		table[elevation[pos] - 1][pos + 0] = pack[r][0][0];
+		table[elevation[pos] - 0][pos + 0] = pack[r][1][0];
+		table[elevation[pos] - 1][pos + 1] = pack[r][0][1];
+		table[elevation[pos] - 0][pos + 1] = pack[r][1][1];
+
+		elevation[pos + 0] += 2;
+		elevation[pos + 1] += 2;
+
+		setCumulative();
 	}
 
 	Field&& getNextField(const Pack& pack, const Command& command) {
 
 		Field next;
+		next.setNextField(pack, command);
 
 		return std::move(next);
+	}
+
+	const Chain simulation() {
+
+		setCumulative();
+
+		int score = 0;
+		int chain = 1;
+
+		while (eraseBlock())
+		{
+			score += static_cast<int>(std::pow(1.3, chain));
+			chain++;
+			dropBlock();
+		}
+
+		return Chain(chain, score);
 	}
 
 	static Field&& Creat() {
@@ -39,6 +373,8 @@ public:
 
 		std::string end;
 		std::cin >> end;
+
+		field.setElevation();
 
 		return std::move(field);
 	}
