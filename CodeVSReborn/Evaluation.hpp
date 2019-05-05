2df168284883e6b70ecaa8731029599bec0d0f7e
ao1415 <ao14159265@gmail.com>
2019-05-05 17:38:23 +0900

    パラメータ調節

--- CodeVSReborn/Evaluation.hpp
+++ CodeVSReborn/Evaluation.hpp
@@ -17,7 +17,7 @@ private:
 		Chain max;
 		double chainScore = 0.0;
 
-		constexpr int DropPos[] = { 0,1,2,7,8,9 };
+		constexpr int DropPos[] = { 0,1,2,3,4,5,6,7,8,9 };
 		//const auto& dropNum = share->packNumber(turn);
 		const int dropNum[] = { 1,2,3,4,5,6,8,9 };
 
